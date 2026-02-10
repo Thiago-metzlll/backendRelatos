8ab9870e5d8f9c24712b5aa830c4a9e3da20933f
Thiago-metzlll <thiagostopassola@gmail.com>
2026-02-10 12:40:03 -0300

    erro de digitação

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:5173', // meu frontend local 
-      'https://front-relatos-two.vercel.app/' // frontend Vercel (prod)
+      'https://front-relatos-two.vercel.app' // frontend Vercel (prod)
     ],
     credentials: true,
   });
